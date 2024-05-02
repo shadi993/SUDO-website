addfd14babe55d0d5a4ba05fd114086147672dda
Petr Seidl <petr.seidl@uss.plus>
2024-05-02 08:50:23 +0200

    feat: added tailwindcss

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,14 @@
+<script>
+	import Footer from '$lib/components/Footer.svelte';
+	import Header from '$lib/components/Header.svelte';
+
+    import '../app.css';
+</script>
+
+<Header />
+
+<main>
+	<slot />
+</main>
+
+<Footer />
