700ae080d13405e0266c207989571879f9e9c7ab
mobius <xxx>
2024-05-06 00:01:24 +0200

    add browser-router and first routes

---
+++ src/Contact.jsx
@@ -0,0 +1,11 @@
+import Menubar from './menubar.jsx'
+import JoinDiscord from './JoinDiscord.jsx'
+import AboutDiscord from './assets/AboutDiscord.jsx'
+
+export const Contact = () => {
+  return (
+    <>
+      <h1>Contact</h1>
+    </>
+  )
+}
--- src/menubar.jsx
+++ src/menubar.jsx
@@ -1,3 +1,4 @@
+import { NavLink } from "react-router-dom";
 function Menubar(){
 
     return(
@@ -7,10 +8,14 @@ function Menubar(){
         <nav className=" dark:bg-gray-800  border-solid border-transparent ">
             <div className=" w-full md:block rounded-l-lg">
                 <ul className=" flex w-full ">
-                    <li><a onClick={()=>Homeclicked()} className=" block py-2 px-3 text-white hover:text-red-600">Home</a></li>
+                    {/* <li><a onClick={()=>Homeclicked()} className=" block py-2 px-3 text-white hover:text-red-600">Home</a></li>
                     <li><a href="#" className=" block py-2 px-3 text-white hover:text-red-600">Projects</a></li>
                     <li><a href="#" className=" block py-2 px-3 text-white hover:text-red-600">Our Team</a></li>
-                    <li><a href="#" className=" block py-2 px-3 text-white hover:text-red-600">Contact</a></li>
+                    <li><a href="#" className=" block py-2 px-3 text-white hover:text-red-600">Contact</a></li> */}
+                    <NavLink to="/home" className=" block py-2 px-3 text-white hover:text-red-600">Home</NavLink>
+                    <NavLink to="/projects" className=" block py-2 px-3 text-white hover:text-red-600">Projects</NavLink>
+                    <NavLink to="/our-team" className=" block py-2 px-3 text-white hover:text-red-600">Our Team</NavLink>
+                    <NavLink to="/contact" className=" block py-2 px-3 text-white hover:text-red-600">Contact</NavLink>
                 </ul>
             </div>
         </nav>
