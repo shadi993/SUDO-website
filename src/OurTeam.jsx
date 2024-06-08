32647e975340bdf91ac4f0980d01f4fb25cd399d
shadi993 <shadihorani@proton.me>
2024-06-09 01:44:23 +0200

    adding team component for Our Team tab, placeholder pics inside avatar folder

--- src/OurTeam.jsx
+++ src/OurTeam.jsx
@@ -1,11 +1,17 @@
 import Menubar from "./menubar"
+import Team from "./team"
 import Footer from "./footer"
 
 export const OurTeam = () => {
     return (
       <>
+        <div id="root" class="flex flex-col min-h-screen dark:border-gray-700 dark:bg-slate-900">
         <Menubar/>
+        <Team/>
+        <div className="absolute bottom-0 w-full ">
         <Footer/>
+        </div>
+        </div>
       </>
     )
 }
